97e056170c574ca4113d99801cf77f0c5e8a56ab
Nguyen Tuan Sieu <ideasvn@hotmail.com>
2016-07-28 16:00:33 +0700

    .

--- src/Model/Permission.php
+++ src/Model/Permission.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
+    protected $table = 'permissions';
+
     protected $fillable = ['name', 'display_name', 'description'];
 
     public function roles() {
--- src/Model/Role.php
+++ src/Model/Role.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    protected $table = 'roles';
+
     protected $fillable = ['name', 'display_name', 'description'];
 
     public function users() {
